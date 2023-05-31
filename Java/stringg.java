f1ee98f70b557c749798efa17f2604fa078866f6
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-01 00:47:55 +0530

    String1

---
+++ Java/stringg.java
@@ -0,0 +1,10 @@
+public class String
+{
+	public static void main(String[] args) {
+	    String s = "Hello all";
+	    System.out.println(s.charAt(3));
+	    System.out.println(s.length());
+	    System.out.println(s.isEmpty());
+	    System.out.println(s.replace(' ','e'));
+	}
+}
