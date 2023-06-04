2307bba4f48dd29e5caac498800eed53fb5e1321
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-06-04 23:09:43 +0530

    String

---
+++ Java/String.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String s = "hello";
+		System.out.println(s.charAt(3));
+		System.out.println(s.length());
+		System.out.println(s.isEmpty());
+	}
+}
+/* Output
+l
+5
+false
+*/
