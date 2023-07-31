1ce697d9751f6714b694215af27cd7606609f26b
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-07-31 21:11:14 +0530

    Alphabet

---
+++ Java/Alphabet.java
@@ -0,0 +1,14 @@
+/*print A to Z Alphabets */
+//A B C ....Z
+
+public class Alphabet {
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
+/* Output
+   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+  */
