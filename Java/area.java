d315dc8c6cbfa17a79c617330477e615efee8817
Amarjeet obroy <115175619+amarjeetobroy@users.noreply.github.com>
2023-07-31 21:12:26 +0530

    Find Area

---
+++ Java/area.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class area {
+    public static void main(String[]args){
+    //    final double PI=3.14,area;
+    //     System.out.println("Enter radious of circle:");
+    //     Scanner sc=new Scanner(System.in);
+    //     int r = sc.nextInt();
+
+    //     area=PI*r*r;
+    //     System.out.println("area of circle:"+area);
+
+
+             // square 
+
+             int area,square;
+             System.out.println("Enter side square:");
+             Scanner sc=new Scanner(System.in);
+             square=sc.nextInt();
+
+            area=square*square;
+            System.out.println("Area of square:"+area);
+             
+     }
+    
+}
+/*
+Output
+  Enter side square:
+2
+Area of square:4
+  */
